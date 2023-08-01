cffa1add05a956df2d5edfa4d807eeffe271ae5e
Sankaranarayanan Thangaraj <csksankar2004@gmail.com>
2023-08-01 21:06:24 +0530

    Sankar-now modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ class HelloWorld
 	public static void main(String args[])
 	{
            System.out.println("Hello World Java");
-	   System.out.println("Hi Guys");
+	   System.out.println("Hi IT Guys");
 	}
 
 }
